7c57ab2f09e6d9cf65e060a5dfbf1084c00277b1
jitaek <jitaek@Lecturer-PC>
2018-12-28 12:37:49 +0900

    loop

--- Java_Basic/src/_02_control/StringLower.java
+++ Java_Basic/src/_02_control/StringLower.java
@@ -6,9 +6,22 @@ public class StringLower {
 	// 해당 문자열의 첫 글자를 소문자로 변경하여 출력하시오.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String m = "Cesktop";
+		String m = "KEskTop";
 		
-		// desktop / cesktop 으로 출력
+		// deskTop / cesktop 으로 출력
+		char ch = m.charAt(0);
+		// 문자열의 첫번째(0) 인덱스의 캐릭터가 대문자인경우,
+		if (ch >= 'A' && ch <= 'Z') {
+			// 대문자 --> 소문자
+			char temp = (char)(ch + 32);
+			String s = m.substring(1);
+			
+			s = temp + s;
+			
+			System.out.println(s);
+		} else {
+			System.out.println(m);
+		}
 	}
 
 }
---
+++ Java_Basic/src/loop/WhileDemo.java
@@ -0,0 +1,48 @@
+package loop;
+
+import java.util.Scanner;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+	
+		// 반복문
+		// 1에서 100까지 정수를 합하는 프로그램
+		// while , for
+		int i = 1;
+		int sum = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		String message = scanner.next();
+//		System.out.println("--> " + message);
+		
+		// 스트링이 숫자인지?
+		int length = message.length();
+		i = 0;
+		boolean isDigit = true;
+		while (i < length) {
+			// 특정 i 인덱스의 캐릭터가 숫자 캐릭터의 범위인지?
+			if (!(message.charAt(i) >= '0' && message.charAt(i) <= '9')) {
+				isDigit = false;
+				break;
+			}
+			
+			i = i + 1;
+		}
+		
+		if (isDigit == true) {
+			int num = Integer.parseInt(message);
+			i = 1;
+			while (i <= num) {
+				sum = sum + i;
+				i = i + 1;
+			}
+			
+			System.out.println(sum);
+		} else {
+			System.out.println("잘못된 입력을 하였습니다.");
+		}
+
+	}
+
+}
