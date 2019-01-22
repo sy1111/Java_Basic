60a86d232381916647d2af571c63d6f24f6a19fd
jitaek <jitaek@Lecturer-PC>
2019-01-22 13:04:46 +0900

    student 1/22

--- Java_Basic/src/student/Main.java
+++ Java_Basic/src/student/Main.java
@@ -4,18 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		Student stu01 = new Student("홍길동");
-		stu01.name = "홍길동";
-		stu01.kScore = 60;
-		stu01.eScore = 55;
-		stu01.mScore= 99;
+		stu01.setkScore(99);
+		stu01.seteScore(90);
+		stu01.setmScore(89);
+		stu01.setName("홍길순");
+		
+		System.out.println(stu01.getName() + ": " +
+				stu01.getkScore() + ", " +
+				stu01.geteScore() + ", " +
+				stu01.getmScore());
 		
 		Student stu02 = new Student("김도완");
-		stu02.name = "김도완";
-		stu02.kScore = 70;
-		stu02.eScore = 85;
-		stu02.mScore= 69;
 		
-		System.out.println(stu02.getSum());
+		 
+		
+		
 	}
 
 }
--- Java_Basic/src/student/Student.java
+++ Java_Basic/src/student/Student.java
@@ -3,29 +3,63 @@ package student;
 // 접근제어자 (access modifier)
 // public / protected / default / private
 class Student {
-
-	String name;
-	int kScore;
-	int eScore;
-	int mScore;
+	// 멤버 변수(variables)
+	private String name;
+	private int kScore;
+	private int eScore;
+	private int mScore;
 	
 	// 메소드
 	// 생성자(constructor)
 	// default 생성자
-	Student() { }
+	//Student() { }
 	
 	Student(String name) {
 		this.name = name;
-		kScore = -1;
-		eScore = -1;
-		mScore = -1;
+		this.kScore = -1;
+		this.eScore = -1;
+		this.mScore = -1;
 	}
 	
 	// 점수의 총합을 리턴
 	// 입력: 
 	// 리턴: int
 	int getSum() {
-		int sum = kScore + eScore + mScore;
+		int sum = this.kScore + this.eScore + this.mScore;
 		return sum;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getkScore() {
+		return kScore;
+	}
+
+	public void setkScore(int kScore) {
+		this.kScore = kScore;
+	}
+
+	public int geteScore() {
+		return eScore;
+	}
+
+	public void seteScore(int eScore) {
+		this.eScore = eScore;
+	}
+
+	public int getmScore() {
+		return mScore;
+	}
+
+	public void setmScore(int mScore) {
+		this.mScore = mScore;
+	}
+	
+	
 }
